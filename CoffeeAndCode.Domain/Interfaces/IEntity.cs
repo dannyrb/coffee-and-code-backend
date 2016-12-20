1b9b59df3178fbd0251a5b3d1e687d76160a6edf
Danny Brown <csengineer13@gmail.com>
2016-12-20 15:21:45 -0500

    Interfaces for entities

---
+++ CoffeeAndCode.Domain/Interfaces/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CoffeeAndCode.Domain.Interfaces
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+}
---
+++ CoffeeAndCode.Domain/Interfaces/INamedEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CoffeeAndCode.Domain.Interfaces
+{
+    public interface INamedEntity: IEntity
+    {
+        string Name { get; set; }
+    }
+}
