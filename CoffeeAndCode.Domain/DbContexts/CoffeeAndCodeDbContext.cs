9553d6c7568e57e1e7bec926dac4882daad699ec
Danny Brown <csengineer13@gmail.com>
2016-12-20 10:27:03 -0500

    init dbcontext class

---
+++ CoffeeAndCode.Domain/DbContexts/CoffeeAndCodeDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeAndCode.Domain.DbContexts
+{
+    public class CoffeeAndCodeDbContext : DbContext
+    {
+        public CoffeeAndCodeDbContext(DbContextOptions<CoffeeAndCodeDbContext> options) : base(options)
+            {
+        }
+
+        //public DbSet<Course> Courses { get; set; }
+        //public DbSet<Enrollment> Enrollments { get; set; }
+        //public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Course>().ToTable("Course");
+            //modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            //modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+
+}
