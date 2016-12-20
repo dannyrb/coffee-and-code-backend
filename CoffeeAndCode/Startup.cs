04bb1b9c5aad6d508b2e2afc1c58194ea6fc11e0
Danny Brown <csengineer13@gmail.com>
2016-12-20 10:40:42 -0500

    Making sure our DB is created/seeded

--- CoffeeAndCode/Startup.cs
+++ CoffeeAndCode/Startup.cs
@@ -37,7 +37,7 @@ namespace CoffeeAndCode
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, CoffeeAndCodeDbContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -49,6 +49,9 @@ namespace CoffeeAndCode
 
             // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
             app.UseSwaggerUi();
+
+            // Create/Seed DB if it doesn't exist, or is empty
+            DbInitializer.Initialize(context);
         }
     }
 }
