1af6de835b6a9a754886a6ed4b14ce55a9984de9
Danny Brown <csengineer13@gmail.com>
2016-12-21 10:21:23 -0500

    Tracking entities by default

--- CoffeeAndCode.Domain/Interfaces/IEntity.cs
+++ CoffeeAndCode.Domain/Interfaces/IEntity.cs
@@ -5,5 +5,10 @@ namespace CoffeeAndCode.Domain.Interfaces
     public interface IEntity
     {
         Guid Id { get; set; }
+
+        // todo: potentially pull out into ITrackedEntity
+        DateTime CreatedDateTime { get; set; }
+        DateTime LastModifiedDateTime { get; set; }
+        DateTime? DeletedDateTime { get; set; }
     }
 }
